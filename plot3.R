cdd65d3b7144044b31cdbfbb55c3e8f6b6146eb5
nadyajdaa <nadyajdaa@gmail.com>
2019-03-04 00:16:49 +0200

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,15 @@
+#Calling ReadingData function that I have previously created in a script called ReadingData.R
+ReadingData()
+
+par(mar= c(4,4,4,4))
+with(DataSet, {
+  plot(Sub_metering_1 ~ datetime,ylab = "Global Active Power (kilowatts)", xlab = "")
+  
+  lines(Sub_metering_2 ~ datetime, col = 'Red')
+  lines(Sub_metering_3 ~ datetime, col = 'Blue')
+  legend("topright",col = c("black", "red", "blue"), lty = 1, lwd = 2, legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3") )
+})
+
+#Saving the Plot into PNG Graphich Device
+dev.copy(png,"plot3.png", width=480, height=480)
+dev.off()
